ffbbb30fc20a24804f1f926ae41b0516ce962bad
Hélder Tavares da Silva <helder.silva@byside.com>
2014-04-12 16:55:49 +0100

    Adds config file. The server port can be configured trough that file.

---
+++ config/config.js
@@ -0,0 +1,16 @@
+/*jslint node: true*/
+
+"use strict";
+
+
+module.exports = {
+    development: {
+        port: 3000
+    },
+    test: {
+        port: 3000
+    },
+    production: {
+        port: 8080
+    }
+};
